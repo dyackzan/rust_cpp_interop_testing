da0d3eb81cb876205f05b6d54974cfc0081b78c3
dyackzan <dwyackzan@gmail.com>
2025-07-16 21:32:48 -0600

    Add simple C++ math lib example

---
+++ main.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+#include "math_cpp/include/math_cpp.hpp"
+
+int main() {
+    std::cout << "4 + 5 = " << add(4, 5) << std::endl;
+    std::cout << "4 * 5 = " << multiply(4, 5) << std::endl;
+    return 0;
+}
