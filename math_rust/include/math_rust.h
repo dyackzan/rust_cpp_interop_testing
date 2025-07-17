520e4e718b9bfaf1ebbafe3b2f20b278ca0699b0
dyackzan <dwyackzan@gmail.com>
2025-07-16 21:34:35 -0600

    Replace C++ math lib with Rust lib

---
+++ math_rust/include/math_rust.h
@@ -0,0 +1,15 @@
+#ifndef MATH_H
+#define MATH_H
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+int32_t add(int32_t a, int32_t b);
+int32_t multiply(int32_t a, int32_t b);
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif // MATH_H
